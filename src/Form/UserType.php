da96fc140d23d0ea842b4ec7001dd5d63c901345
LudovicDaub <ludovicdaub@sfr.fr>
2022-10-01 17:24:28 +0200

    create user form

---
+++ src/Form/UserType.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+
+        $user = $options['data'] ?? null;
+        $isEdit = $user && $user->getId();
+
+        $isEdit = $options['isEdit'];
+
+        $builder
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Type d\'utilisateur',
+                'required' => true,
+                'disabled' => true,
+                'multiple' => false,
+                'attr' => [
+                    'class' => 'form-select',
+                ],
+                'choices'  => [
+                    'Partenaire' => 'ROLE_PARTENAIRE',
+                    'Structure' => 'ROLE_STRUCTURE'
+                ],
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Nom de l\'utilisateur',
+                'required' => true,
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 60
+                ]),
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email du partenaire',
+                'required' => true,
+                'constraints' => new Length([
+                    'min' => 2,
+                    'max' => 60
+                ]),
+                'attr' => [
+                    'placeholder' => 'Merci de saisir une adresse email'
+                ]
+            ])
+            ->add('isActive', CheckboxType::class, [
+                'label' => false,
+                'label_attr' => ['class' => 'switch-custom is-active-btn'],
+                'required' => false,
+            ]);
+
+
+        // Affichage conditionnel du Password.
+        // On vérifie si on se trouve dans le cas ou l’option isEdit est à true. Si ce n’est pas le cas, alors j’affiche l’édition du mot de passe sinon je l’ignore.
+        if (!$isEdit) {
+            $builder->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identiques',
+                'label' => false,
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir votre mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir un mot de passe'
+                    ]
+                ],
+            ]);
+        };
+
+        // Data transformer for Roles array
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    return count($rolesArray) ? $rolesArray[0] : null;
+                },
+                function ($rolesString) {
+                    return [$rolesString];
+                }
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'isEdit' => false,
+            'csrf_protection' => true,
+        ]);
+
+        $resolver->setAllowedTypes('isEdit', 'bool');
+    }
+}
