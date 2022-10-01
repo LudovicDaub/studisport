c9c93a0bd2e82b9bb47f3df698d48d5727803832
LudovicDaub <ludovicdaub@sfr.fr>
2022-10-01 17:23:11 +0200

    create structure form

---
+++ src/Form/StructureFormShowType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Structure;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
+class StructureFormShowType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('postalAdress', TextType::class, [
+        'label' => 'Adresse postale de la structure',
+        'disabled' => true,
+      ])
+      ->add('partner', TextType::class, [
+        'disabled' => true,
+      ]);
+  }
+
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+      'data_class' => Structure::class,
+    ]);
+  }
+}
