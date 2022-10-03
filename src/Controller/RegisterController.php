1930deef349ce94475d4c3c76012adb654ecb66c
LudovicDaub <ludovicdaub@sfr.fr>
2022-10-03 09:29:01 +0200

    create register controller

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[IsGranted('ROLE_ADMIN')]
+class RegisterController extends AbstractController
+{
+    public function __construct(
+        public EntityManagerInterface $entityManager
+        ) {}
+
+    #[Route('/inscription', name: 'app_register')]
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+
+        $user = new User(); // J'instancie ma classe User(), j'ai donc un nouvel objet User
+        $form = $this->createForm(RegisterType::class, $user); // J'instancie mon formulaire avec la méthode createForm... (en paramètres : la classe du formulaire, et l'objet User )
+
+        $form->handleRequest($request); // Écoute la requête entrante
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Injecte dans mon objet User() toutes les données qui sont récupérées du formulaire
+            $user = $form->getData();
+            
+            // J'utilise UserPasswordHasherInterface pour encoder le mot de passe
+            $password = $passwordHasher->hashPassword($user, $user->getPassword());
+            // Je réinjecte $password qui est crypté dans l'objet User()
+            $user->setPassword($password);
+
+            // Je fige la data avant de l'enregistrer
+            $this->entityManager->persist($user);
+            // Execute la persistance et enregistre la en base de données
+            $this->entityManager->flush();
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
